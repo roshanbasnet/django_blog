edc1fad74de0db46712aa6a64d9e4cba00bfcfae
roshanbasnet <roshanbasnet26@gmail.com>
2022-05-31 12:59:09 -0400

    added creat,edit, delete function for blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,14 +4,20 @@ from . import  views
 from .views import (
     PostListView,
     PostDetailView,
-    PostCreateView
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',PostListView.as_view(),name='blog-home'),
+    path('user/<str:username>',UserPostListView.as_view(),name='user-posts'),
     path('post/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
     path('post/new/',PostCreateView.as_view(),name='post-create'),
+    path('post/<int:pk>/update',PostUpdateView.as_view(),name='post-update'),
+    path('post/<int:pk>/delete',PostDeleteView.as_view(),name='post-delete'),
     # path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView,
+    DeleteView
+
 )
 
 
@@ -24,6 +29,18 @@ class PostListView(ListView):
     # ordering the post from newest to oldest by adding (-) on
     # on the ordering field
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 # To get single view
@@ -32,9 +49,41 @@ class PostDetailView(DetailView):
 
 
 # To get single view
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# To get single view
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
